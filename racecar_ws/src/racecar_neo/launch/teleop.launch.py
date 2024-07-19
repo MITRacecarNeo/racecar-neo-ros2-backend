4ee07aa4421d992cffd7ce79500be3b293d67e2a
Chris Lai <chrisclai02@gmail.com>
2024-07-19 01:41:02 -0400

    include rplidar launch in teleop

--- racecar_ws/src/racecar_neo/launch/teleop.launch.py
+++ racecar_ws/src/racecar_neo/launch/teleop.launch.py
@@ -38,14 +38,26 @@ def generate_launch_description():
         executable='camera',
         name='camera_node'
 	),
-	Node (
-		package='racecar_neo',
-		executable='decode_camera',
-		name='image_decoder'
-	),
+	#Node (
+		#package='racecar_neo',
+		#executable='decode_camera',
+		#name='image_decoder'
+	#),
     Node(
 	    package='racecar_neo',
         executable='imu',
         name='imu_node'
 	),
+	Node(
+            package='sllidar_ros2',
+            executable='sllidar_node',
+            name='sllidar_node',
+            parameters=[{'channel_type':'serial',
+                         'serial_port': '/dev/ttyUSB0', 
+                         'serial_baudrate':115200, 
+                         'frame_id': 'laser',
+                         'inverted':False, 
+                         'angle_compensate':True}],
+            output='screen'
+    ),
     ])
